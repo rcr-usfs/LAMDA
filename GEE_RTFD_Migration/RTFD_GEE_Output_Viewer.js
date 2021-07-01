1844e7d5ab2ff3d0076c0d009b82285f97bf1682
Your Name <you@example.com>
2021-07-01 16:59:37 -0600

    AK and HI updates and viewer updates

--- GEE_RTFD_Migration/RTFD_GEE_Output_Viewer.js
+++ GEE_RTFD_Migration/RTFD_GEE_Output_Viewer.js
@@ -1,9 +1,37 @@
+var app = {};
+
+//-------- Map Panel-----------------
+app.map = ui.Map();
+app.map.style().set({cursor:'crosshair'});
+app.map.setOptions('HYBRID');
+
+app.optionsFiltersPanel = ui.Panel({
+  layout: ui.Panel.Layout.flow('vertical'),
+  style: {width: '250px', position: 'top-left'}
+});
+app.plotPanel = ui.Panel({
+  layout: ui.Panel.Layout.flow('vertical'),
+  style: {width: '400',position:'top-right'}
+});
+app.ZdownloadPanel = ui.Panel({
+  layout: ui.Panel.Layout.flow('vertical'),
+  style: {width: '225px',margin:'5px',border : '1px solid black'}
+});
+app.TDDdownloadPanel = ui.Panel({
+  layout: ui.Panel.Layout.flow('vertical'),
+  style: {width: '225',margin:'5px',border : '1px solid black'}
+});
+app.legendPanel = ui.Panel(null, null, {position: 'bottom-right',margin:'5px'});
+app.chartPanel = ui.Panel(null, null, {stretch: 'horizontal'});
+app.blackline = function(){return ui.Label('__________________________________________')};
+app.wideBlackline = function(){return ui.Label('____________________________________________________________________________')};
+
 //https://storage.googleapis.com/storage/v1/b/rtfd-scratch/o
 var analysisYears = [2020];
 
 var nDays = 16;
 
-var startJulians = ee.List.sequence(65,321,8).getInfo();
+var startJulians = ee.List.sequence(209,233,8).getInfo();//ee.List.sequence(65,321,8).getInfo();
 
 var zBaselineLength = 3;
 
@@ -27,6 +55,8 @@ var exportArea = ee.FeatureCollection('projects/lcms-292214/assets/CONUS-Ancilla
 
 var lossDurPalette = 'FF0,F00,ff00c3';//'0C2780,E2F400,BD1600';
 var lossMagPalette = 'D00,F5DEB3';
+
+
 ///////////////////////////////////////////////////////////////////////
 analysisYears.map(function(ay){
   var ts = [];
@@ -36,12 +66,12 @@ analysisYears.map(function(ay){
     // indexNames.map(function(indexName){
       
       var baselineStartYear = ay-baselineGap-zBaselineLength;
-	    var baselineEndYear = ay-baselineGap -1;
-	    
-	    var tddStartYear =  ay-epochLength+1;
-	    var tddEndYear = ay;
-	    
-	    var nameZ = 'bl'+baselineStartYear.toString() + '-'+baselineEndYear.toString()+
+      var baselineEndYear = ay-baselineGap -1;
+      
+      var tddStartYear =  ay-epochLength+1;
+      var tddEndYear = ay;
+      
+      var nameZ = 'bl'+baselineStartYear.toString() + '-'+baselineEndYear.toString()+
       '_ay'+ay.toString() + 
       '_jd'+startJulian.toString() + '-'+endJulian.toString();
       var urlZ = bucket + '/' +runName + '_RTFD_Z_'+indexName+ '_'+nameZ
@@ -51,14 +81,18 @@ analysisYears.map(function(ay){
       '_jd'+startJulian.toString() + '-'+endJulian.toString();
       var urlTDD = bucket + '/' +runName + '_RTFD_TDD_'+indexName+ '_'+nameTDD
        + '.tif';
-      
+      var storageAPIUrlZ= urlZ.replace('gs:/','https://storage.googleapis.com');
+      var storageAPIUrlTDD= urlTDD.replace('gs:/','https://storage.googleapis.com');
+      app.ZdownloadPanel.add(ui.Label('RTFD Z '+ay.toString() + ' '+startJulian.toString() + '-'+endJulian.toString(), null, storageAPIUrlZ));
+      app.TDDdownloadPanel.add(ui.Label('RTFD TDD '+tddStartYear.toString() + '-' + tddEndYear.toString() + ' '+startJulian.toString() + '-'+endJulian.toString(),null,storageAPIUrlTDD));
+      // app.ZdownloadPanel.add(app.blackline());
       var imgZ = ee.Image.loadGeoTIFF(urlZ);
       imgZ = imgZ.updateMask(imgZ.neq(-32768)).divide(1000).clip(exportArea);
-      Map.addLayer(imgZ,{min:-3,max:3,palette:'F00,888,00F'},'Z '+nameZ,false);
+      app.map.addLayer(imgZ,{min:-3,max:3,palette:'F00,888,00F'},'Z '+nameZ,false);
       
       var imgTDD = ee.Image.loadGeoTIFF(urlTDD);
       imgTDD = imgTDD.updateMask(imgTDD.neq(-32768)).divide(100000).clip(exportArea);
-      Map.addLayer(imgTDD,{min:-0.05,max:0.05,palette:'F00,888,00F'},'TDD '+nameTDD,false);
+      app.map.addLayer(imgTDD,{min:-0.05,max:0.05,palette:'F00,888,00F'},'TDD '+nameTDD,false);
       
       ts.push(ee.Image.cat([imgZ,imgTDD]).rename(['z','tdd']).float().set(
         {'system:time_start':date.millis(),
@@ -72,10 +106,10 @@ analysisYears.map(function(ay){
         var change = tst.map(function(img){return img.lte(ee.Image([zThresh,slopeThresh])).selfMask()});
         var changeCount = change.count();
         var changeMag = tst.reduce(persistenceReducer).updateMask(changeCount.mask());
-        Map.addLayer(changeCount.select([0]),{min:1,max:persistencePeriod,palette:lossDurPalette},'Z Persistence Count '+startJulian.toString() + '-'+endJulian.toString(),false);
-        Map.addLayer(changeMag.select([0]),{min:-4,max:-1,palette:lossMagPalette},'Z Persistence Mag '+startJulian.toString() + '-'+endJulian.toString(),false);
-        Map.addLayer(changeCount.select([1]),{min:1,max:persistencePeriod,palette:lossDurPalette},'TDD Persistence Count '+startJulian.toString() + '-'+endJulian.toString(),false);
-        Map.addLayer(changeMag.select([1]),{min:-0.1,max:-0.02,palette:lossMagPalette},'TDD Persistence Mag '+startJulian.toString() + '-'+endJulian.toString(),false)
+        app.map.addLayer(changeCount.select([0]),{min:1,max:persistencePeriod,palette:lossDurPalette},'Z Persistence Count '+startJulian.toString() + '-'+endJulian.toString(),false);
+        app.map.addLayer(changeMag.select([0]),{min:-4,max:-1,palette:lossMagPalette},'Z Persistence Mag '+startJulian.toString() + '-'+endJulian.toString(),false);
+        app.map.addLayer(changeCount.select([1]),{min:1,max:persistencePeriod,palette:lossDurPalette},'TDD Persistence Count '+startJulian.toString() + '-'+endJulian.toString(),false);
+        app.map.addLayer(changeMag.select([1]),{min:-0.1,max:-0.02,palette:lossMagPalette},'TDD Persistence Mag '+startJulian.toString() + '-'+endJulian.toString(),false)
       }
       // print(ts.length)
     });
@@ -85,5 +119,78 @@ analysisYears.map(function(ay){
 });
 
 
+// app.downloadSelect = ui.Select(app.downloadList,'Select Output to Download',null,app.downloadSelectedOutput)
+app.ZdownloadPanelShown = false;
+app.TDDdownloadPanelShown = false;
+app.togglePanel = function(containerID,panelID,panelShown,index){
+  if(panelShown){
+    containerID.remove(panelID);
+    panelShown = false;
+  }else{
+    containerID.insert(index,panelID);
+    panelShown = true;
+  }
+  return panelShown;
+};
+app.toggleZDownloadPanel = function(){
+  app.ZdownloadPanelShown =app.togglePanel(app.optionsFiltersPanel,app.ZdownloadPanel,app.ZdownloadPanelShown,2);
+};
+app.toggleTDDDownloadPanel = function(){
+  app.TDDdownloadPanelShown =app.togglePanel(app.optionsFiltersPanel,app.TDDdownloadPanel,app.TDDdownloadPanelShown,3);
+};
+app.toggleZDownloadPanelButton = ui.Button('Toggle RTFD Z Downloads',app.toggleZDownloadPanel,false,{padding:'0px',margin:'5px',fontSize:'5px',position:'top-left'});
+app.toggleTDDDownloadPanelButton = ui.Button('Toggle RTFD TDD Downloads',app.toggleTDDDownloadPanel,false,{padding:'0px',margin:'5px',fontSize:'5px',position:'top-left'});
+app.optionsFiltersPanel.add(app.toggleZDownloadPanelButton);
+app.optionsFiltersPanel.add(app.toggleTDDDownloadPanelButton);
+// app.optionsFiltersPanel.add(app.ZdownloadPanel);
+// app.optionsFiltersPanel.add(app.TDDdownloadPanel);
+// print(app.downloadList)
+/////////////////////////////////////////////////////////////
 
-Map.setOptions('HYBRID');
+app.filtersShown = true;
+app.plotsShown = true;
+app.hideFilters = function(){
+  ui.root.remove(app.optionsFiltersPanel);
+  app.toggleFiltersButton.setLabel('-->');
+  app.filtersShown = false;
+};
+app.showFilters = function(){
+  ui.root.insert(0,app.optionsFiltersPanel);
+  app.toggleFiltersButton.setLabel('<--');
+  app.filtersShown = true;
+};
+app.toggleFilters = function(){
+  if(app.filtersShown){
+    app.hideFilters();
+  }else{app.showFilters()}
+}
+
+app.hidePlotPanel = function(){
+  ui.root.remove(app.plotPanel);
+  app.togglePlotPanelButton.setLabel('<--');
+  app.plotsShown = false;
+};
+app.showPlotPanel = function(){
+  ui.root.insert(2,app.plotPanel);
+  app.togglePlotPanelButton.setLabel('-->');
+  app.plotsShown = true;
+};
+app.togglePlotPanel = function(){
+  if(app.plotsShown){
+    app.hidePlotPanel();
+  }else{app.showPlotPanel()}
+};
+app.toggleFiltersButton = ui.Button('<--',app.toggleFilters,false,{padding:'0px',margin:'0px',fontSize:'5px',position:'top-left'});
+app.togglePlotPanelButton = ui.Button('-->',app.togglePlotPanel,false,{padding:'0px',margin:'0px',fontSize:'5px',position:'top-right'});
+
+app.map.add(app.toggleFiltersButton);
+app.map.add(app.togglePlotPanelButton);
+
+/////////////////////////////////////////////////////////////
+app.plotPanel.add(app.wideBlackline());
+//Put app together
+ui.root.clear();
+ui.root.add(app.optionsFiltersPanel);
+
+ui.root.add(app.map);
+ui.root.add(app.plotPanel);
--- GEE_RTFD_Migration/RTFD_GEE_Template.py
+++ GEE_RTFD_Migration/RTFD_GEE_Template.py
@@ -118,8 +118,56 @@ transform_dict = {'AK': [240,0,-51375,0,-240,1512585],
                   'HI': [240,0,-342585,0,-240,2127135],
                   'CONUS': [240,0,-2361915.0,0,-240,3177735.0]
                   }
-export_area_dict = {'AK':ee.FeatureCollection("TIGER/2018/States").filter(ee.Filter.eq('NAME','Alaska')),
-                    'HI':ee.FeatureCollection("TIGER/2018/States").filter(ee.Filter.eq('NAME','Hawaii')),
+hi_study_area = ee.Geometry.Polygon([
+    [
+      [
+        -178.44361349674858,
+        18.865478602779188
+      ],
+      [
+        -154.75578456980554,
+        18.865478602779188
+      ],
+      [
+        -154.75578456980554,
+        28.517270853283037
+      ],
+      [
+        -178.44361349674858,
+        28.517270853283037
+      ],
+      [
+        -178.44361349674858,
+        18.865478602779188
+      ]
+    ]
+  ],None,False)
+ak_study_area = ee.Geometry.Polygon([
+    [
+      [
+        172.34783450770118,
+        51.17507314490824
+      ],
+      [
+        230.02585011272546,
+        51.17507314490824
+      ],
+      [
+        230.02585011272546,
+        71.43978827307926
+      ],
+      [
+        172.34783450770118,
+        71.43978827307926
+      ],
+      [
+        172.34783450770118,
+        51.17507314490824
+      ]
+    ]
+  ],None,False)
+export_area_dict = {'AK':ak_study_area,
+                    'HI':hi_study_area,
                     'CONUS': ee.FeatureCollection('projects/lcms-292214/assets/CONUS-Ancillary-Data/conus')}
 
 #Export area name - provide a descriptive name for the study area
@@ -152,7 +200,7 @@ exportTDDOutputs = False
 #           [-83.62254283932539, 45.13859495600731]]], None, False)
 exportArea = export_area_dict[exportAreaName]
 #Whether to add prelim outputs to map to view
-runGEEViz = True	
+runGEEViz = False	
 
 #######################################################
 #Set up tree masks
@@ -168,6 +216,7 @@ lcmsTreeMask = lcms.select(['Land_Cover']).map(lambda img: img.lte(6)).max().sel
 # Map.addLayer(lcmsChange,{'min':1,'max':1,'palette':'800'},'LCMS Change',False)
 # Map.addLayer(lcmsTreeMask,{'min':1,'max':1,'palette':'080','classLegendDict':{'Trees':'080'}},'LCMS Trees',False)
 
+#Pull in AK and HI tree masks (no LCMS currently available across all of AK or any of HI)
 akTreeMask = ee.Image('projects/gtac-rtfd/assets/Ancillary/AK_forest_mask')
 hiTreeMask = ee.Image("USGS/NLCD_RELEASES/2016_REL/2016_HI").gte(10).selfMask()
 # Map.addLayer(akTreeMask,{'min':1,'max':1,'palette':'080','classLegendDict':{'Trees':'080'}},'AK FIA Trees',False)
@@ -179,7 +228,9 @@ tree_mask = tree_mask_dict[exportAreaName]
 ####################################################################################################
 # rtfd_wrapper(analysisYears, startJulians, nDays , zBaselineLength, tddEpochLength, baselineGap , indexNames,zThresh,slopeThresh,zReducer, tddAnnualReducer,zenithThresh,addLookAngleBands,applyCloudScore, applyTDOM,cloudScoreThresh,performCloudScoreOffset,cloudScorePctl, zScoreThresh, shadowSumThresh, contractPixels,dilatePixels,resampleMethod,preComputedCloudScoreOffset,preComputedTDOMIRMean,preComputedTDOMIRStdDev, tree_mask,crs,transform, scale,exportBucket,exportAreaName,exportArea,exportRawZ,exportRawSlope,exportZOutputs,exportTDDOutputs)
 
-computeCloudScoreTDOMStats(2015,2020,120,280,exportArea,cloudScoreTDOMStatsDir,exportAreaName,crs,transform)
+#Compute cloudScore and TDOM stats if they are not available
+computeCloudScoreTDOMStats(2020,2020,152,212,exportArea,cloudScoreTDOMStatsDir,exportAreaName,crs,transform)
+
 if runGEEViz:
 	Map.view()
 
